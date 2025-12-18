a553e1fd5478cb49c70880169cc68b23ab9ed866
K.Dileepa Thushan Peiris <dileepapeiris5@gmail.com>
2025-12-18 10:42:46 +0530

    Add support center UI components
    Introduces new React components for the support center, including SupportHeader, OngoingCasesList (with CaseListHeader, CaseListItem, and CaseListFooter), and updates the Support index export. These components provide the structure and UI for displaying support cases and related actions in the customer portal.

---
+++ apps/customer-portal/webapp/src/components/Support/index.ts
@@ -0,0 +1,7 @@
+export * from "./Header/SupportHeader";
+export * from "./Stats/SupportStatsRow";
+export * from "./StartChatBanner/NewChatCTA";
+export * from "./OngoingCasesList/index";
+export * from "./ChatHistory/ChatHistoryList";
+export * from "./ActionCards/ServiceRequestCard";
+export * from "./ActionCards/ChangeRequestCard";
